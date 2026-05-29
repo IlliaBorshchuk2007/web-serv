3d89bf8f9ff98fd50549b445e84c00f211cc728b
IlliaBorshchuk2007 <illabor37@gmail.com>
2026-05-29 11:41:59 +0000

    lab7

--- app/Http/Controllers/Api/Blog/Admin/CategoryController.php
+++ app/Http/Controllers/Api/Blog/Admin/CategoryController.php
@@ -3,15 +3,21 @@
 namespace App\Http\Controllers\Api\Blog\Admin;
 
 use App\Models\BlogCategory;
+use App\Repositories\BlogCategoryRepository;
 use App\Http\Requests\BlogCategoryUpdateRequest;
 use App\Http\Requests\BlogCategoryCreateRequest;
 use Illuminate\Support\Str;
 
 class CategoryController extends BaseController
 {
+    public function __construct(private BlogCategoryRepository $blogCategoryRepository)
+    {
+        //parent::__construct();
+    }
+
     public function index()
     {
-        $paginator = BlogCategory::paginate(5);
+        $paginator = $this->blogCategoryRepository->getAllWithPaginate(5);
         return $paginator;
     }
 
@@ -37,7 +43,7 @@ class CategoryController extends BaseController
 
     public function update(BlogCategoryUpdateRequest $request, $id)
     {
-        $item = BlogCategory::find($id);
+        $item = $this->blogCategoryRepository->getEdit($id);
         if (empty($item)) {
             return ['message' => "Запис id=[{$id}] не знайдено"];
         }
