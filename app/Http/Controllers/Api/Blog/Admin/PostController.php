9464c576538c2bf53ea99aada8a652a142e97f4f
IlliaBorshchuk2007 <illabor37@gmail.com>
2026-05-29 12:02:01 +0000

    lab9

--- app/Http/Controllers/Api/Blog/Admin/PostController.php
+++ app/Http/Controllers/Api/Blog/Admin/PostController.php
@@ -3,17 +3,53 @@
 namespace App\Http\Controllers\Api\Blog\Admin;
 
 use App\Repositories\BlogPostRepository;
+use App\Repositories\BlogCategoryRepository;
+use App\Http\Requests\BlogPostUpdateRequest;
+use Carbon\Carbon;
+use Illuminate\Support\Str;
 
 class PostController extends BaseController
 {
+    private BlogCategoryRepository $blogCategoryRepository;
+
     public function __construct(private BlogPostRepository $blogPostRepository)
     {
         //parent::__construct();
+        $this->blogCategoryRepository = new BlogCategoryRepository();
     }
 
     public function index()
     {
         $paginator = $this->blogPostRepository->getAllWithPaginate();
         return $paginator;
+    }
+
+    public function update(BlogPostUpdateRequest $request, $id)
+    {
+        $item = $this->blogPostRepository->getEdit($id);
+        if (empty($item)) {
+            return ['message' => "Запис id=[{$id}] не знайдено"];
+        }
+
+        $data = $request->all();
+
+        if (empty($data['slug'])) {
+            $data['slug'] = Str::slug($data['title']);
+        }
+
+        if (empty($data['published_at']) && $data['is_published']) {
+            $data['published_at'] = Carbon::now();
+        }
+
+        $result = $item->update($data);
+
+        if ($result) {
+            return [
+                'success' => true,
+                'message' => 'Успішно збережено'
+            ];
+        } else {
+            return ['message' => 'Помилка збереження'];
+        }
     }
 }
